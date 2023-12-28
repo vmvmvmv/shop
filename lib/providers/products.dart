676b059e0f54fa2fee4dbcec409ab1b0249796dc
vmvmvmv <>
2023-12-28 17:08:21 +0200

    init

---
+++ lib/providers/products.dart
@@ -0,0 +1,143 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
+import './product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  final String authToken;
+  final String userId;
+
+  Products(this.authToken, this.userId, this._items);
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    var params = {'auth': authToken};
+
+    if (filterByUser) {
+      params['orderBy'] = "creatorId";
+      params['equalTo'] = userId;
+    }
+
+    var url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products.json',
+        params);
+
+    try {
+      final response = await http.get(url);
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      if (extractedData == null) {
+        return;
+      }
+      url = Uri.https(
+          'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+          '/userFavorites/$userId.json',
+          {'auth': authToken});
+      final favoriteResponse = await http.get(url);
+      final favoriteData = json.decode(favoriteResponse.body);
+      final List<Product> loadedProducts = [];
+      extractedData.forEach((prodId, prodData) {
+        loadedProducts.add(Product(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          isFavorite:
+              favoriteData == null ? false : favoriteData[prodId] ?? false,
+          imageUrl: prodData['imageUrl'],
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+    } catch (error) {
+      throw (error);
+    }
+  }
+
+  Future<void> addProduct(Product product) async {
+    final url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products.json',
+        {'auth': authToken});
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'imageUrl': product.imageUrl,
+          'price': product.price,
+          'creatorId': userId,
+        }),
+      );
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      _items.add(newProduct);
+
+      notifyListeners();
+    } catch (error) {
+      print(error);
+      throw error;
+    }
+  }
+
+  Future<void> updateProduct(String id, Product newProduct) async {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      final url = Uri.https(
+          'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+          '/products/$id.json',
+          {'auth': authToken});
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price
+          }));
+      _items[prodIndex] = newProduct;
+      notifyListeners();
+    } else {
+      print('...');
+    }
+  }
+
+  Future<void> deleteProduct(String id) async {
+    final url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products/$id.json',
+        {'auth': authToken});
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
+    notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Could not delete product.');
+    }
+    existingProduct = null;
+  }
+}
